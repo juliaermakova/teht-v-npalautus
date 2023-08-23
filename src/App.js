9f6c48263c4ebe9143b5924d4441a125f833c492
Julia Ermakova <115168570+juliaermakova@users.noreply.github.com>
2023-08-23 10:44:13 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header course={course}/>
+      <Content
+        part1= {part1} exercises1 ={exercises1}
+        part2= {part2} exercises2 ={exercises2}
+        part3= {part3} exercises3 ={exercises3}
+      />
+      <Total
+        exercises1 ={exercises1}
+        exercises2 ={exercises2}
+        exercises3 ={exercises3}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+// uudet komponentit: Header, Content ja Total
+// HEADER: kurssin nimen renderöinti
+const Header = ({course}) => {
+
+  return (
+    <div>
+      <h1>{course}</h1>
+    </div>
+  )
+}
+
+// CONTENT: osat ja niiden tehtävämäärät
+const Content = ({part1, exercises1, part2, exercises2, part3, exercises3}) => {
+
+  return (
+    <div>
+      <p>{part1} {exercises1}</p>
+      <p>{part2} {exercises2}</p>
+      <p>{part3} {exercises3}</p>
+    </div>
+  )
+}
+
+// TOTAL: tehtävien yhteismäärä
+const Total = ({exercises1, exercises2, exercises3}) => {
+
+  return (
+    <div>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+export default App
